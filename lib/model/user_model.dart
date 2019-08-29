9b4871465580f42f8c660b765189b94d2aab7d18
Justwen <yyhshrrr@126.com>
2019-08-29 15:09:16 +0800

    增加sp的支持，将cookie存至sp中

--- lib/model/user_model.dart
+++ lib/model/user_model.dart
@@ -1,7 +1,11 @@
+import 'dart:convert';
+
+import 'package:shared_preferences/shared_preferences.dart';
+
 class UserModel {
   static UserModel _sInstance;
 
-  List<UserInfo> userList = new List();
+  List<UserInfo> userList;
 
   static UserModel getInstance() {
     if (_sInstance == null) {
@@ -14,7 +18,15 @@ class UserModel {
     _initialize();
   }
 
-  _initialize() {}
+  _initialize() async {
+    SharedPreferences sp = await SharedPreferences.getInstance();
+    String data = sp.getString("user");
+    List<dynamic> list = jsonDecode(data);
+    userList = new List();
+    list.forEach((item) {
+      userList.add(UserInfo.fromJson(item));
+    });
+  }
 
   void addUser(String uName, String uid, String cid) {
     for (UserInfo user in userList) {
@@ -23,6 +35,7 @@ class UserModel {
       }
     }
     userList.add(new UserInfo(uName, uid, cid));
+    _save();
   }
 
   bool isEmpty() {
@@ -36,6 +49,13 @@ class UserModel {
       return "";
     }
   }
+
+  void _save() async {
+    SharedPreferences sp = await SharedPreferences.getInstance();
+    String data = jsonEncode(userList);
+    print(data);
+    sp.setString("user", data);
+  }
 }
 
 class UserInfo {
@@ -46,4 +66,23 @@ class UserInfo {
   String uid;
 
   String cid;
+
+  Map toJson() {
+    Map map = new Map();
+    map["uName"] = this.uName;
+    map["value"] = this.cid;
+    map["cid"] = this.cid;
+    return map;
+  }
+
+  factory UserInfo.fromJson(Map<String, dynamic> srcJson) {
+    UserInfo userInfo =
+        new UserInfo(srcJson["uName"], srcJson["value"], srcJson["cid"]);
+    return userInfo;
+  }
+
+  @override
+  String toString() {
+    return toJson().toString();
+  }
 }
