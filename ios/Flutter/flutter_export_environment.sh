c5b060641945c5d469ae0741a9a44bfaf34c1dc3
Justwen <yyhshrrr@126.com>
2019-10-06 23:13:25 +0800

    ios环境修改

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=F:\Flutter"
-export "FLUTTER_APPLICATION_PATH=F:\Coding\github\NGA-CLIENT-VER-OPEN-SOURCE-Flutter"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_ROOT=/Users/yangyihang/Dev/Flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/yangyihang/Dev/Github/NGA-CLIENT-VER-OPEN-SOURCE-Flutter"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=F:\Flutter\bin\cache\artifacts\engine\ios"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/yangyihang/Dev/Flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
